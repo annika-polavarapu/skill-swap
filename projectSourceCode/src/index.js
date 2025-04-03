63789b77e33ebfeaccca0ee30a3f25e477dacb16
Evan Mohan <prooflion@gmail.com>
2025-04-02 23:17:46 -0600

    Home page is now just / for simplicity and fanciness & docker correctly displays webpage

--- projectSourceCode/src/index.js
+++ projectSourceCode/src/index.js
@@ -76,9 +76,13 @@ app.use(
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 
+app.get('/', (req, res) => {
+  res.render('pages/home'); 
+});
+
 app.get('/home', (req, res) => {
-    res.redirect('pages/home'); 
-  });
+    res.redirect('/'); 
+});
 
 
 
